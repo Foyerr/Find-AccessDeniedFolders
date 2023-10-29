b7e8850f5bc3c32d40166b8dad4ea4cf89f5b1a8
Walker <43224440+Foyerr@users.noreply.github.com>
2023-10-29 20:54:13 -0300

    Update Find-AccessDeniedFolders.ps1
    Added exclusions, removed last write-host to allow for piping to other functions

--- Find-AccessDeniedFolders.ps1
+++ Find-AccessDeniedFolders.ps1
@@ -1,38 +1,61 @@
-﻿# Define the root directory to start the search
-$rootDirectory = "W:\"
+
+<#
+.SYNOPSIS
+    Recursively searches for folders starting from a specified root directory and identifies those with "Access Denied" errors.
+
+.PARAMETER path
+    Specifies the directory path where the search will begin.
+    [ValidateScript: Must be a valid path]
+    [Default: None]
+
+.PARAMETER exclude
+    Specifies an array of directory paths to be excluded from the search.
+    [Default: None]
+
+.EXAMPLE
+    Find-AccessDeniedFolders -path "W:\" -exclude @("W:\System Volume Information", "W:\$RECYCLE.BIN")
+    This example starts the search from the "W:\" directory, excludes specified directories, and lists folders where access is denied.
+
+.NOTES
+    - The function uses Get-ChildItem to traverse the directory structure.
+    - It employs a try-catch block to capture "Access Denied" errors and other unexpected errors.
+    - The function writes the paths with "Access Denied" errors to the standard output.
+    - Directory exclusions are handled using the Where-Object cmdlet to filter out specified directories.
+#>
+
 
 # Function to recursively find folders with access denied errors
 function Find-AccessDeniedFolders {
     param (
-        [string]$path
+        [ValidateScript({Test-Path $_})]
+        [string]$path=$null,
+        [string[]]$exclude=$null
     )
 
     try {
         # Try to get the child items in the current directory
-        $items = Get-ChildItem -Directory -Path $path -ErrorAction Stop
+        $items = Get-ChildItem -Directory -Path $path -ErrorAction Stop | Where-Object { $_.FullName -notin $exclude }
 
         foreach ($item in $items) {
-            # Check if the item is a directory (folder)
-           
             # Continue recursively searching in subdirectories
-            Find-AccessDeniedFolders -path $item.FullName
-           
+            Find-AccessDeniedFolders -path $item.FullName -exclude $exclude
         }
     }
     catch {
         # Check if the error message contains "Access to the path is denied"
         if ($_ -match "Access to the path '(.+)' is denied.") {
             $accessDeniedPath = $matches[1]
-            
-            #Show that the SID for depracatied group is assigned to directory
-            #Get-Acl $accessDeniedPath | select AccessToString 
-            
-            Write-Output $accessDeniedPath #| Out-File -Append -FilePath $accessDeniedFoldersFile
+            Write-Output $accessDeniedPath
+        }
+        else {
+            Write-Host "An unexpected error occurred: $_"
         }
     }
 }
 
-# Start the search from the root directory
-Find-AccessDeniedFolders -path $rootDirectory
+# Define the root directory to start the search
+$rootDirectory = "W:\"
+$excludedDirectories = @("W:\System Volume Information", "W:\$RECYCLE.BIN")  # Add directories to be excluded
 
-Write-Host "Search completed. Folders with access denied errors are listed in $accessDeniedFoldersFile."
+# Start the search from the root directory
+Find-AccessDeniedFolders -path $rootDirectory -exclude $excludedDirectories
